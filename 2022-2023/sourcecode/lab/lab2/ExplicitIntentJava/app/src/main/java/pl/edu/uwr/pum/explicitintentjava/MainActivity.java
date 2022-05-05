c06d20b00807d0c1aadb5fbda59d603a74e6eb08
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-05-05 22:33:50 +0200

    ExplicitIntentJava data transfer to second activity

--- 2022-2023/sourcecode/lab/lab2/ExplicitIntentJava/app/src/main/java/pl/edu/uwr/pum/explicitintentjava/MainActivity.java
+++ 2022-2023/sourcecode/lab/lab2/ExplicitIntentJava/app/src/main/java/pl/edu/uwr/pum/explicitintentjava/MainActivity.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "pl.edu.uwr.pum.explicitintentjava.MESSAGE";
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextMain);
     }
 
     public void startSecondActivity(View view) {
+        String message = editText.getText().toString();
         Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
--- 2022-2023/sourcecode/lab/lab2/ExplicitIntentJava/app/src/main/java/pl/edu/uwr/pum/explicitintentjava/SecondActivity.java
+++ 2022-2023/sourcecode/lab/lab2/ExplicitIntentJava/app/src/main/java/pl/edu/uwr/pum/explicitintentjava/SecondActivity.java
@@ -2,7 +2,9 @@ package pl.edu.uwr.pum.explicitintentjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
