19c3adeae396bfefa3386fc0bbf165e20301623b
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-05-06 22:25:33 +0200

    explicitIntentKotlin onActivityResult

--- 2022-2023/sourcecode/lab/lab2/ExplicitIntentKotlin/app/src/main/java/pl/edu/uwr/pum/explicitintentkotlin/MainActivity.kt
+++ 2022-2023/sourcecode/lab/lab2/ExplicitIntentKotlin/app/src/main/java/pl/edu/uwr/pum/explicitintentkotlin/MainActivity.kt
@@ -1,10 +1,13 @@
 package pl.edu.uwr.pum.explicitintentkotlin
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 const val EXTRA_MESSAGE = "pl.edu.uwr.pum.explicitintentkotlin.MESSAGE"
 
@@ -17,11 +20,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    private val secondActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if (result.resultCode == Activity.RESULT_OK)
+            findViewById<TextView>(R.id.textViewMain).text =
+                result.data!!.getStringExtra(EXTRA_REPLY)
+    }
+
     fun startSecondActivity(view: View) {
         val message = editText.text.toString()
         val intent = Intent(this, SecondActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
         }
-        startActivity(intent)
+
+        secondActivityResultLauncher.launch(intent)
     }
 }
--- 2022-2023/sourcecode/lab/lab2/ExplicitIntentKotlin/app/src/main/java/pl/edu/uwr/pum/explicitintentkotlin/SecondActivity.kt
+++ 2022-2023/sourcecode/lab/lab2/ExplicitIntentKotlin/app/src/main/java/pl/edu/uwr/pum/explicitintentkotlin/SecondActivity.kt
@@ -1,10 +1,18 @@
 package pl.edu.uwr.pum.explicitintentkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
+const val EXTRA_REPLY = "pl.edu.uwr.pum.explicitintentkotlin.REPLY"
+
 class SecondActivity : AppCompatActivity() {
+
+    private val editText: EditText by lazy { findViewById(R.id.editTextSecond) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -13,5 +21,15 @@ class SecondActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.textView).apply {
             text = message
         }
+    }
+
+    fun returnMessage(view: View) {
+        setResult(
+            RESULT_OK,
+            Intent().apply {
+                putExtra(EXTRA_REPLY,
+                    findViewById<EditText>(R.id.editTextSecond).text.toString())
+            })
+        finish()
     }
 }
