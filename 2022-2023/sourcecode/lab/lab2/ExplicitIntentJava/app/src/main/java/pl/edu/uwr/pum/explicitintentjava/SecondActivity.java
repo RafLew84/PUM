ef07be1a4c92b54f5042a53608db28a7b6b33c5f
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-05-06 20:41:29 +0200

    ExplicitIntentJava onActivityResult

--- 2022-2023/sourcecode/lab/lab2/ExplicitIntentJava/app/src/main/java/pl/edu/uwr/pum/explicitintentjava/SecondActivity.java
+++ 2022-2023/sourcecode/lab/lab2/ExplicitIntentJava/app/src/main/java/pl/edu/uwr/pum/explicitintentjava/SecondActivity.java
@@ -4,18 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "pl.edu.uwr.pum.explicitintentjava.REPLY";
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        editText = findViewById(R.id.editTextSecond);
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+    }
+
+    public void returnMessage(View view) {
+        String reply = editText.getText().toString();
+        Intent intent = new Intent();
+
+        intent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
