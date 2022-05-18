0ecd67e864c44888c6ca4fe020cf0f2b2043e38a
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-05-19 01:19:41 +0200

    RecyclerViewWordListJava app

--- 2022-2023/sourcecode/lab/lab3/RecyclerViewWordListJava/app/src/main/java/pl/edu/uwr/pum/recyclerviewwordlistjava/MainActivity.java
+++ 2022-2023/sourcecode/lab/lab3/RecyclerViewWordListJava/app/src/main/java/pl/edu/uwr/pum/recyclerviewwordlistjava/MainActivity.java
@@ -1,14 +1,28 @@
 package pl.edu.uwr.pum.recyclerviewwordlistjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> wordList = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 100; i++)
+            wordList.add("Word" + i);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        WordListAdapter wordListAdapter = new WordListAdapter(this, wordList);
+        recyclerView.setAdapter(wordListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ 2022-2023/sourcecode/lab/lab3/RecyclerViewWordListJava/app/src/main/java/pl/edu/uwr/pum/recyclerviewwordlistjava/WordListAdapter.java
@@ -0,0 +1,61 @@
+package pl.edu.uwr.pum.recyclerviewwordlistjava;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordListViewHolder> {
+
+    private LinkedList<String> wordList;
+    private final LayoutInflater inflater;
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        inflater = LayoutInflater.from(context);
+        this.wordList = wordList;
+    }
+
+    @NonNull
+    @Override
+    public WordListAdapter.WordListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = inflater.inflate(R.layout.word_list_item, parent, false);
+        return new WordListViewHolder(itemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordListViewHolder holder, int position) {
+        String current = wordList.get(position);
+        holder.wordText.setText(current);
+    }
+
+    @Override
+    public int getItemCount() {
+        return wordList.size();
+    }
+
+    protected class WordListViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView wordText;
+        final WordListAdapter adapter;
+
+        public WordListViewHolder(@NonNull View itemView, WordListAdapter adapter) {
+            super(itemView);
+            this.adapter = adapter;
+            wordText = itemView.findViewById(R.id.singleWord);
+
+            itemView.setOnClickListener(view -> {
+                int position = getLayoutPosition();
+                String element = wordList.get(position);
+                wordList.set(position, "Clicked!" + element);
+                adapter.notifyItemChanged(position);
+            });
+        }
+    }
+}
