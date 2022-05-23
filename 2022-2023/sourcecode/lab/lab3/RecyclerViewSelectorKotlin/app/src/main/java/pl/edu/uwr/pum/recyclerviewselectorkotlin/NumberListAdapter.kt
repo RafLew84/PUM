5db3b70656bdaeca83973ae023533bee68bbb755
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-05-23 18:56:53 +0200

    recyclerView with Selector in Kotlin

---
+++ 2022-2023/sourcecode/lab/lab3/RecyclerViewSelectorKotlin/app/src/main/java/pl/edu/uwr/pum/recyclerviewselectorkotlin/NumberListAdapter.kt
@@ -0,0 +1,61 @@
+package pl.edu.uwr.pum.recyclerviewselectorkotlin
+
+import android.view.LayoutInflater
+import androidx.recyclerview.selection.DefaultSelectionTracker
+import androidx.recyclerview.selection.SelectionTracker
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.widget.RecyclerView
+
+class NumberListAdapter(
+    private val numberList: List<Int>,
+) : RecyclerView.Adapter<NumberListAdapter.NumberListViewHolder>() {
+
+    lateinit var selectionTracker: SelectionTracker<Long>
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NumberListViewHolder {
+        return NumberListViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.rv_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: NumberListViewHolder, position: Int) {
+        holder.bind(numberList[position], selectionTracker.isSelected(position.toLong()))
+    }
+
+    override fun getItemCount() = numberList.size
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    class NumberListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val number: TextView = itemView.findViewById((R.id.numberText))
+
+        fun bind(value: Int, isActivated: Boolean = false) {
+            number.text = value.toString()
+            itemView.isActivated = isActivated
+        }
+
+        fun getItemDetails(): ItemDetailsLookup.ItemDetails<Long> =
+            object : ItemDetailsLookup.ItemDetails<Long>() {
+                override fun getPosition(): Int = adapterPosition
+                override fun getSelectionKey(): Long = itemId
+            }
+    }
+
+}
