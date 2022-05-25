81ad30ef768a08e6d0456b3b2a74d100e5e79fed
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-05-26 01:51:49 +0200

    WFiApp java

--- 2022-2023/sourcecode/lab/lab3/WfiAppJava/app/src/main/java/pl/edu/uwr/pum/wfiappjava/DetailActivity.java
+++ 2022-2023/sourcecode/lab/lab3/WfiAppJava/app/src/main/java/pl/edu/uwr/pum/wfiappjava/DetailActivity.java
@@ -2,7 +2,12 @@ package pl.edu.uwr.pum.wfiappjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView instituteTitle = findViewById(R.id.titleDetail);
+        ImageView instituteImage = findViewById(R.id.instituteImageDetail);
+
+        Institute institute = getIntent().getParcelableExtra(InstituteAdapter.INSTITUTE_EXTRA);
+
+        if(institute != null) {
+            instituteTitle.setText(institute.getTitle());
+
+            Glide.with(this)
+                    .load(institute.getImageResource())
+                    .into(instituteImage);
+        }
     }
 }
--- 2022-2023/sourcecode/lab/lab3/WfiAppJava/app/src/main/java/pl/edu/uwr/pum/wfiappjava/MainActivity.java
+++ 2022-2023/sourcecode/lab/lab3/WfiAppJava/app/src/main/java/pl/edu/uwr/pum/wfiappjava/MainActivity.java
@@ -1,22 +1,59 @@
 package pl.edu.uwr.pum.wfiappjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<Institute> mInstituteData = new ArrayList<>();
+    private final ArrayList<Institute> institutes = new ArrayList<>();
+    private InstituteAdapter instituteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.fabRefreshButton)
+                .setOnClickListener(view -> {
+                    initializeData();
+                    instituteAdapter.notifyDataSetChanged();
+                });
+
+        RecyclerView mRecyclerView = findViewById(R.id.recyclerView);
+        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,
+                gridColumnCount));
+
+        instituteAdapter = new InstituteAdapter(this, institutes);
+        mRecyclerView.setAdapter(instituteAdapter);
+        initializeData();
     }
 
     private void initializeData(){
+        String[] instituteList = getResources().getStringArray(R.array.institute_titles);
+        String[] instituteInfo = getResources().getStringArray(R.array.institute_info);
 
+        TypedArray instituteImageResources = getResources()
+                .obtainTypedArray(R.array.institute_images);
+
+        institutes.clear();
+
+        for(int i = 0; i < instituteList.length; i++)
+            institutes.add(new Institute(
+                            instituteList[i],
+                            instituteInfo[i],
+                            instituteImageResources.getResourceId(i,0))
+            );
+
+        instituteImageResources.recycle();
     }
 }
