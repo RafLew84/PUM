31f780ac5da42b5454c47c87bcb367fc1c27f753
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-06-08 00:14:28 +0200

    Lab 2 - QuizApp - kotlin

---
+++ 2022-2023/sourcecode/lab/lab2/QuizAppKotlin/app/src/main/java/pl/edu/uwr/pum/quizappkotlin/Question.kt
@@ -0,0 +1,10 @@
+package pl.edu.uwr.pum.quizappkotlin
+
+class Question(
+    val question: String,
+    val imageSource: Int,
+    val answerOne: String,
+    val answerTwo: String,
+    val answerThree: String,
+    val correctAnswer: Int
+)
