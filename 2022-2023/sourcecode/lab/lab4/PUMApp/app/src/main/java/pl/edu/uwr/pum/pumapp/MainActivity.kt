b448dda6f612b465d1a052efb71af9c20c7b8a91
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-07-18 18:34:58 +0200

    lab 4.5 - kotlin PUMApp - nawigacja

---
+++ 2022-2023/sourcecode/lab/lab4/PUMApp/app/src/main/java/pl/edu/uwr/pum/pumapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package pl.edu.uwr.pum.pumapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+        navHostFragment.findNavController()
+    }
+
+    private val appBarConfiguration: AppBarConfiguration by lazy {
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayoutID)
+        AppBarConfiguration(navController.graph, drawerLayout)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        findViewById<NavigationView>(R.id.navigation_drawerID).setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
---
+++ 2022-2023/sourcecode/lab/lab4/PUMApp/app/src/main/java/pl/edu/uwr/pum/pumapp/fragments/StartFragment.kt
@@ -0,0 +1,18 @@
+package pl.edu.uwr.pum.pumapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pl.edu.uwr.pum.pumapp.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+}
