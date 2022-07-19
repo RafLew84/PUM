87c5f21cc097f2640d60c951811633191823525c
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-07-19 09:04:24 +0200

    lab 4.5 - kotlin PUMApp - settings

--- 2022-2023/sourcecode/lab/lab4/PUMApp/app/src/main/java/pl/edu/uwr/pum/pumapp/fragments/SettingsFragment.kt
+++ 2022-2023/sourcecode/lab/lab4/PUMApp/app/src/main/java/pl/edu/uwr/pum/pumapp/fragments/SettingsFragment.kt
@@ -5,15 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import pl.edu.uwr.pum.pumapp.R
 
 class SettingsFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val activity: AppCompatActivity = activity as AppCompatActivity
+        activity.supportActionBar?.title = getString(R.string.settings)
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textView: TextView = view.findViewById(R.id.textViewTheme)
+        view.findViewById<RadioGroup>(R.id.radioButtonTheme).setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId){
+                R.id.radioButtonLight -> {
+                    textView.text = getString(R.string.motywJ)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                R.id.radioButtonDark -> {
+                    textView.text = getString(R.string.motywD)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+            }
+        }
     }
 }
