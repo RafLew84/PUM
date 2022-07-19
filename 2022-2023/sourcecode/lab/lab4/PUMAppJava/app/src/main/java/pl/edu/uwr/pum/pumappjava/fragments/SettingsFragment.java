34512ff7395f74e3643a473b7246b98ad44bc003
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-07-19 09:16:01 +0200

    lab 4.5 java PUMApp - settings

--- 2022-2023/sourcecode/lab/lab4/PUMAppJava/app/src/main/java/pl/edu/uwr/pum/pumappjava/fragments/SettingsFragment.java
+++ 2022-2023/sourcecode/lab/lab4/PUMAppJava/app/src/main/java/pl/edu/uwr/pum/pumappjava/fragments/SettingsFragment.java
@@ -2,20 +2,50 @@ package pl.edu.uwr.pum.pumappjava.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import pl.edu.uwr.pum.pumappjava.R;
 
 public class SettingsFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        ActionBar actionBar = null;
+        if (activity != null) {
+            actionBar = activity.getSupportActionBar();
+        }
+        if (actionBar != null) {
+            actionBar.setTitle(getString(R.string.settings));
+        }
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = view.findViewById(R.id.textViewTheme);
+        RadioGroup radioGroup = view.findViewById(R.id.radioButtonTheme);
+        radioGroup.setOnCheckedChangeListener((r, checkedId) -> {
+            if (checkedId == R.id.radioButtonLight){
+                textView.setText(getString(R.string.motywJ));
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else if (checkedId == R.id.radioButtonDark){
+                textView.setText(getString(R.string.motywD));
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        });
     }
 }
