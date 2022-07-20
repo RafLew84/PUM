4ef362ac71c2faabee300822ed9976a477b9f42b
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-07-20 11:24:16 +0200

    lab 4.5 PUMApp - StartFragment

--- 2022-2023/sourcecode/lab/lab4/PUMApp/app/src/main/java/pl/edu/uwr/pum/pumapp/fragments/StartFragment.kt
+++ 2022-2023/sourcecode/lab/lab4/PUMApp/app/src/main/java/pl/edu/uwr/pum/pumapp/fragments/StartFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import pl.edu.uwr.pum.pumapp.R
 
 class StartFragment : Fragment() {
@@ -13,6 +16,15 @@ class StartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val activity: AppCompatActivity = activity as AppCompatActivity
+        activity.supportActionBar?.title = getString(R.string.app_name)
         return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<LinearLayout>(R.id.start_button_layout).setOnClickListener {
+            findNavController().navigate(StartFragmentDirections.actionStartFragmentToModuleListFragment())
+        }
     }
 }
--- 2022-2023/sourcecode/lab/lab4/PUMAppJava/app/src/main/java/pl/edu/uwr/pum/pumappjava/fragments/StartFragment.java
+++ 2022-2023/sourcecode/lab/lab4/PUMAppJava/app/src/main/java/pl/edu/uwr/pum/pumappjava/fragments/StartFragment.java
@@ -2,19 +2,42 @@ package pl.edu.uwr.pum.pumappjava.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import pl.edu.uwr.pum.pumappjava.R;
 
 public class StartFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        ActionBar actionBar = null;
+        if (activity != null) {
+            actionBar = activity.getSupportActionBar();
+        }
+        if (actionBar != null) {
+            actionBar.setTitle(getString(R.string.app_name));
+        }
         return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LinearLayout button = view.findViewById(R.id.start_button_layout);
+        button.setOnClickListener(view1 -> {
+            Navigation.findNavController(view).navigate(StartFragmentDirections.actionStartFragmentToModuleListFragment());
+        });
     }
 }
