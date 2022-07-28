51dcdd67166525a2fd1dcfdd0379bbe3ae456d7f
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-07-28 19:10:19 +0200

    lab 5.2 - TemplateFragment.kt - poprawa literówek

--- 2022-2023/sourcecode/lab/lab5/ViewPagerFragmentStateAdapterKotlin/app/src/main/java/pl/edu/uwr/pum/viewpagerfragmentstateadapterkotlin/TemplateFragment.kt
+++ 2022-2023/sourcecode/lab/lab5/ViewPagerFragmentStateAdapterKotlin/app/src/main/java/pl/edu/uwr/pum/viewpagerfragmentstateadapterkotlin/TemplateFragment.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class TemplateFragment (val position: Int) : Fragment() {
+class TemplateFragment (private val position: Int) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
