8e7038c25c8dededcece98d42ec81cf2458bf6f7
Rafał Lewandków <r.lewandkow@gmail.com>
2022-07-31 19:47:34 +0200

    Update Bill.kt

--- 2022-2023/sourcecode/lab/lab5/MyFinanceAppKotlin/app/src/main/java/pl/edu/uwr/pum/myfinanceappkotlin/data/Bill.kt
+++ 2022-2023/sourcecode/lab/lab5/MyFinanceAppKotlin/app/src/main/java/pl/edu/uwr/pum/myfinanceappkotlin/data/Bill.kt
@@ -1,11 +1,10 @@
 package pl.edu.uwr.pum.myfinanceappkotlin.data
 
 import java.time.LocalDate
-import java.util.*
 
 data class Bill (
     val name: String,
     val endDate: LocalDate,
     val amount: Double,
     val color: Int
-        )
+)
