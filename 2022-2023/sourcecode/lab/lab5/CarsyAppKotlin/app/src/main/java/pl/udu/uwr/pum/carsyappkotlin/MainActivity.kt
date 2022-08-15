70a3d545c144d084be13af61d9074c22c4d4a4e2
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2022-08-15 23:06:50 +0200

    Revert "Carsy - drawer Navigation"

--- 2022-2023/sourcecode/lab/lab5/CarsyAppKotlin/app/src/main/java/pl/udu/uwr/pum/carsyappkotlin/MainActivity.kt
+++ 2022-2023/sourcecode/lab/lab5/CarsyAppKotlin/app/src/main/java/pl/udu/uwr/pum/carsyappkotlin/MainActivity.kt
@@ -1,18 +1,12 @@
 package pl.udu.uwr.pum.carsyappkotlin
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.core.content.ContextCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.*
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,27 +16,11 @@ class MainActivity : AppCompatActivity() {
         navHostFragment.findNavController()
     }
 
-    private val appBarConfiguration: AppBarConfiguration by lazy {
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayoutID)
-        AppBarConfiguration(navController.graph, drawerLayout)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar!!.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.dark_blue_900, null)))
-        findViewById<BottomNavigationView>(R.id.bottom_nav_view).setupWithNavController(navController)
-        findViewById<NavigationView>(R.id.navigation_drawerID).setupWithNavController(navController)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
     }
 }
