d16eb8b5c19ae43d081caa624566eed4a25f5e20
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2022-08-15 16:33:06 +0200

    Carsy - drawer Navigation

--- 2022-2023/sourcecode/lab/lab5/CarsyAppJava/app/src/main/java/pl/udu/uwr/pum/carsyappjava/MainActivity.java
+++ 2022-2023/sourcecode/lab/lab5/CarsyAppJava/app/src/main/java/pl/udu/uwr/pum/carsyappjava/MainActivity.java
@@ -1,17 +1,26 @@
 package pl.udu.uwr.pum.carsyappjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,27 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayoutID);
 
+        appBarConfiguration =  new AppBarConfiguration.Builder(navController.getGraph())
+                .setOpenableLayout(drawerLayout)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationView navigationView = findViewById(R.id.navigation_drawerID);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(
+                new ColorDrawable(getResources().getColor(R.color.dark_blue_900, null)));
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 }
--- 2022-2023/sourcecode/lab/lab5/CarsyAppKotlin/app/src/main/java/pl/udu/uwr/pum/carsyappkotlin/MainActivity.kt
+++ 2022-2023/sourcecode/lab/lab5/CarsyAppKotlin/app/src/main/java/pl/udu/uwr/pum/carsyappkotlin/MainActivity.kt
@@ -1,12 +1,18 @@
 package pl.udu.uwr.pum.carsyappkotlin
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,11 +22,27 @@ class MainActivity : AppCompatActivity() {
         navHostFragment.findNavController()
     }
 
+    private val appBarConfiguration: AppBarConfiguration by lazy {
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayoutID)
+        AppBarConfiguration(navController.graph, drawerLayout)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.dark_blue_900, null)))
+        findViewById<BottomNavigationView>(R.id.bottom_nav_view).setupWithNavController(navController)
+        findViewById<NavigationView>(R.id.navigation_drawerID).setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
-        bottomNav?.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
