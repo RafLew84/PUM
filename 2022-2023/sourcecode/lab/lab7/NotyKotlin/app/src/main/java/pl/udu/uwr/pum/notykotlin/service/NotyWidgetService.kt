6f51b975255aa74eddd4b650928161e0388f66c1
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2022-08-26 19:12:14 +0200

    Noty - kotlin - basics

---
+++ 2022-2023/sourcecode/lab/lab7/NotyKotlin/app/src/main/java/pl/udu/uwr/pum/notykotlin/service/NotyWidgetService.kt
@@ -0,0 +1,63 @@
+package pl.udu.uwr.pum.notykotlin.service
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+import pl.udu.uwr.pum.notykotlin.R
+import pl.udu.uwr.pum.notykotlin.data.DataProvider
+
+class NotyWidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
+        return NotyWidgetItemFactory(applicationContext, intent)
+    }
+
+    internal class NotyWidgetItemFactory(private val context: Context, intent: Intent) :
+        RemoteViewsFactory {
+        private val appWidgetId: Int
+        private lateinit var noteList: List<String>
+        override fun onCreate() {
+            // otworz baze danych
+            noteList = DataProvider.dummyData
+        }
+
+        override fun onDataSetChanged() {}
+        override fun onDestroy() {
+            // zamknac polaczenie z baza
+        }
+
+        override fun getCount(): Int {
+            return noteList.size
+        }
+
+        override fun getViewAt(position: Int): RemoteViews {
+            val remoteViews = RemoteViews(context.packageName, R.layout.item_list)
+            remoteViews.setTextViewText(R.id.itemListTextView, noteList[position])
+            return remoteViews
+        }
+
+        override fun getLoadingView(): RemoteViews? {
+            return null
+        }
+
+        override fun getViewTypeCount(): Int {
+            return 1
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong() // identyfikacja
+        }
+
+        override fun hasStableIds(): Boolean {
+            return true
+        }
+
+        init {
+            appWidgetId = intent.getIntExtra(
+                AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID
+            )
+        }
+    }
+}
