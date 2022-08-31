bb48859292ab055ea1b7eb1c868407049d1478e4
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2022-08-31 15:28:02 +0200

    lab 8.1 - viemodel

---
+++ 2022-2023/sourcecode/lab/lab8/ViewModelBasicsJava/app/src/main/java/pl/udu/uwr/pum/viewmodelbasicsjava/viemodel/ScrambleViewModel.java
@@ -0,0 +1,85 @@
+package pl.udu.uwr.pum.viewmodelbasicsjava.viemodel;
+
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import pl.udu.uwr.pum.viewmodelbasicsjava.data.DataProvider;
+
+public class ScrambleViewModel extends ViewModel {
+    private int currentWordCount = 0;
+    private int score = 0;
+    private String currentWord;
+    private final ArrayList<String> usedWordsList = new ArrayList<>();
+    private String currentScrambledWord;
+
+    public ScrambleViewModel () {
+        super();
+        getNextWord();
+    }
+
+    public boolean nextWord() {
+        if (currentWordCount < DataProvider.MAX_NO_OF_WORDS) {
+            getNextWord();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isUserWordCorrect(String playerWord) {
+        if (playerWord.equals(currentWord)) {
+            increaseScore();
+            return true;
+        }
+        return false;
+    }
+
+    public void reinitializeData() {
+        score = 0;
+        currentWordCount = 0;
+        usedWordsList.clear();
+        getNextWord();
+    }
+
+    private void getNextWord() {
+        Random rand = new Random();
+        currentWord = DataProvider.allWordsList.get(rand.nextInt(DataProvider.allWordsList.size()));;
+        char[] tempWord = currentWord.toCharArray();
+        while (String.valueOf(tempWord).equals(currentWord)){
+            for (int i = tempWord.length - 1; i > 0; i--){
+                int j = (int) (Math.random() * (i + 1));
+
+                char temp = tempWord[i];
+                tempWord[i] = tempWord[j];
+                tempWord[j] = temp;
+            }
+        }
+        if (usedWordsList.contains(currentWord)) getNextWord();
+        else {
+            currentScrambledWord = Arrays.toString(tempWord)
+                    .replace(",", "")
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(" ", "")
+                    .trim();
+            ++currentWordCount;
+            usedWordsList.add(currentWord);
+        }
+    }
+
+    private void increaseScore() {
+        score += DataProvider.SCORE_INCREASE;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getCurrentScrambledWord() {
+        return currentScrambledWord;
+    }
+}
---
+++ 2022-2023/sourcecode/lab/lab8/ViewModelBasicsKotlin/app/src/main/java/pl/udu/uwr/pum/viewmodelbasicskotlin/viewmodel/ScrambleViewModel.kt
@@ -0,0 +1,62 @@
+package pl.udu.uwr.pum.viewmodelbasicskotlin.viewmodel
+
+import androidx.lifecycle.ViewModel
+import pl.udu.uwr.pum.viewmodelbasicskotlin.data.MAX_NO_OF_WORDS
+import pl.udu.uwr.pum.viewmodelbasicskotlin.data.SCORE_INCREASE
+import pl.udu.uwr.pum.viewmodelbasicskotlin.data.allWordsList
+
+class ScrambleViewModel : ViewModel() {
+    private var currentWordCount = 0
+
+    private lateinit var _currentScrambledWord: String
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    private var usedWordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private var _score = 0
+    val score: Int
+        get() = _score
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    fun reinitializeData() {
+        _score = 0
+        currentWordCount = 0
+        usedWordsList.clear()
+        getNextWord()
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        while (String(tempWord) == currentWord) tempWord.shuffle()
+        if (usedWordsList.contains(currentWord)) getNextWord() else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            usedWordsList.add(currentWord)
+        }
+    }
+
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+
+    init {
+        getNextWord()
+    }
+}
