ba7ed1efcbc88bdf903907c18d08512f87e629df
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2022-09-01 18:07:09 +0200

    lab 8.3 - java - databinding

---
+++ 2022-2023/sourcecode/lab/lab8/DataBindingBasicsJava/app/src/main/java/pl/udu/uwr/pum/databindingbasicsjava/MainActivity.java
@@ -0,0 +1,16 @@
+package pl.udu.uwr.pum.databindingbasicsjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.udu.uwr.pum.databindingbasicsjava.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ 2022-2023/sourcecode/lab/lab8/DataBindingBasicsJava/app/src/main/java/pl/udu/uwr/pum/databindingbasicsjava/viemodel/ScrambleViewModel.java
@@ -0,0 +1,92 @@
+package pl.udu.uwr.pum.databindingbasicsjava.viemodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import pl.udu.uwr.pum.databindingbasicsjava.data.DataProvider;
+
+public class ScrambleViewModel extends ViewModel {
+    private final MutableLiveData<Integer> currentWordCount = new MutableLiveData<>(0);
+    private final MutableLiveData<Integer> score = new MutableLiveData<>(0);
+    private String currentWord;
+    private final ArrayList<String> usedWordsList = new ArrayList<>();
+    private final MutableLiveData<String> currentScrambledWord = new MutableLiveData<>();
+
+    public ScrambleViewModel () {
+        super();
+        getNextWord();
+    }
+
+    public boolean nextWord() {
+            if (currentWordCount.getValue() != null &&
+                    currentWordCount.getValue() < DataProvider.MAX_NO_OF_WORDS) {
+                getNextWord();
+                return true;
+            }
+            return false;
+    }
+
+    public boolean isUserWordCorrect(String playerWord) {
+        if (playerWord.equals(currentWord)) {
+            increaseScore();
+            return true;
+        }
+        return false;
+    }
+
+    public void reinitializeData() {
+        score.setValue(0);
+        currentWordCount.setValue(0);
+        usedWordsList.clear();
+        getNextWord();
+    }
+
+    private void getNextWord() {
+        Random rand = new Random();
+        currentWord = DataProvider.allWordsList.get(rand.nextInt(DataProvider.allWordsList.size()));;
+        char[] tempWord = currentWord.toCharArray();
+        if (usedWordsList.contains(currentWord)) getNextWord();
+        else {
+            do{
+                for (int i = tempWord.length - 1; i > 0; i--){
+                    int j = (int) (Math.random() * (i + 1));
+
+                    char temp = tempWord[i];
+                    tempWord[i] = tempWord[j];
+                    tempWord[j] = temp;
+                }
+            }while (String.valueOf(tempWord).equals(currentWord));
+            currentScrambledWord.setValue(Arrays.toString(tempWord)
+                    .replace(",", "")
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(" ", "")
+                    .trim());
+            if (currentWordCount.getValue() != null)
+                currentWordCount.setValue(currentWordCount.getValue() + 1);
+            usedWordsList.add(currentWord);
+        }
+    }
+
+    private void increaseScore() {
+        if (score.getValue() != null)
+            score.setValue(score.getValue() + DataProvider.SCORE_INCREASE);
+    }
+
+    public LiveData<Integer> getScore() {
+        return score;
+    }
+
+    public LiveData<String> getCurrentScrambledWord() {
+        return currentScrambledWord;
+    }
+
+    public LiveData<Integer> getCurrentWordCount() {
+        return currentWordCount;
+    }
+}
