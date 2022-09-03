7e9c834377c92b65c3cd7cd5d20f3aa42d7bbf40
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-09-03 21:11:35 +0200

    lab 9.1 - kotlin - viewholder fix

--- 2022-2023/sourcecode/lab/lab9/ROOMbasicsKotlin/app/src/main/java/pl/udu/uwr/pum/roombasicskotlin/view/WordListAdapter.kt
+++ 2022-2023/sourcecode/lab/lab9/ROOMbasicsKotlin/app/src/main/java/pl/udu/uwr/pum/roombasicskotlin/view/WordListAdapter.kt
@@ -1,13 +1,17 @@
 package pl.udu.uwr.pum.roombasicskotlin.view
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import pl.udu.uwr.pum.roombasicskotlin.R
 import pl.udu.uwr.pum.roombasicskotlin.model.Word
 
 class WordListAdapter(wordsComparator: WordsComparator) : ListAdapter<Word, WordViewHolder>(wordsComparator) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-        return WordViewHolder.create(parent)
+        return WordViewHolder(LayoutInflater.from(parent.context).inflate(
+            R.layout.recyclerview_item, parent, false
+        ))
     }
 
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
--- 2022-2023/sourcecode/lab/lab9/ROOMbasicsKotlin/app/src/main/java/pl/udu/uwr/pum/roombasicskotlin/view/WordViewHolder.kt
+++ 2022-2023/sourcecode/lab/lab9/ROOMbasicsKotlin/app/src/main/java/pl/udu/uwr/pum/roombasicskotlin/view/WordViewHolder.kt
@@ -1,8 +1,6 @@
 package pl.udu.uwr.pum.roombasicskotlin.view
 
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import pl.udu.uwr.pum.roombasicskotlin.R
@@ -12,13 +10,5 @@ class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(text: String?) {
         wordItemView.text = text
-    }
-
-    companion object {
-        fun create(parent: ViewGroup): WordViewHolder {
-            val view: View = LayoutInflater.from(parent.context)
-                .inflate(R.layout.recyclerview_item, parent, false)
-            return WordViewHolder(view)
-        }
     }
 }
