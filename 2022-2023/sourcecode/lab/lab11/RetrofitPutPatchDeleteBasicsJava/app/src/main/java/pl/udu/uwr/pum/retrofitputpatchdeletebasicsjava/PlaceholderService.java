adc4eae411c75d70e8fe9239811b74e24f1c197d
Rafal Lewandkow <r.lewandkow@gmail.com>
2022-09-12 17:25:52 +0200

    lab 11.4 - kotlin add unit type

--- 2022-2023/sourcecode/lab/lab11/RetrofitPutPatchDeleteBasicsJava/app/src/main/java/pl/udu/uwr/pum/retrofitputpatchdeletebasicsjava/PlaceholderService.java
+++ 2022-2023/sourcecode/lab/lab11/RetrofitPutPatchDeleteBasicsJava/app/src/main/java/pl/udu/uwr/pum/retrofitputpatchdeletebasicsjava/PlaceholderService.java
@@ -1,15 +1,10 @@
 package pl.udu.uwr.pum.retrofitputpatchdeletebasicsjava;
 
-import java.util.Map;
-
+import kotlin.Unit;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
-import retrofit2.http.Field;
-import retrofit2.http.FieldMap;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.PATCH;
-import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
@@ -22,5 +17,5 @@ public interface PlaceholderService {
     Call<Post> patchPost(@Path("id") int postId, @Body Post post);
 
     @DELETE("posts/{id}")
-    Call<Void> deletePost(@Path("id") int postId);
+    Call<Unit> deletePost(@Path("id") int postId);
 }
