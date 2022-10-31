3d8bb5d83dae4bdaca4117e796c39f052f6b24a1
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2022-10-31 12:21:10 +0100

    PolishNewsApp - kotlin - nawigacja

---
+++ 2022-2023/sourcecode/lab/lab13/PolishNewsApp/app/src/main/java/pl/udu/uwr/pum/polishnewsapp/ui/MainActivity.kt
@@ -0,0 +1,43 @@
+package pl.udu.uwr.pum.polishnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import pl.udu.uwr.pum.polishnewsapp.R
+import pl.udu.uwr.pum.polishnewsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+        navHostFragment.findNavController()
+    }
+
+    private val appBarConfiguration: AppBarConfiguration by lazy {
+        AppBarConfiguration(setOf(R.id.favoriteNewsFragment, R.id.latestNewsFragment, R.id.searchNewsFragment))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.bottomNavView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
