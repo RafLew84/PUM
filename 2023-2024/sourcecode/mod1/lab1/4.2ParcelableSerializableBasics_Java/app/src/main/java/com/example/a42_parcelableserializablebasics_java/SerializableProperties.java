020478214ae43d81fa0ce1dcb20f04c4257906a4
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-03-26 19:38:29 +0200

    mod1 - lab1 - Parcelable, Serializable Basics - Java

---
+++ 2023-2024/sourcecode/mod1/lab1/4.2ParcelableSerializableBasics_Java/app/src/main/java/com/example/a42_parcelableserializablebasics_java/SerializableProperties.java
@@ -0,0 +1,39 @@
+package com.example.a42_parcelableserializablebasics_java;
+
+import java.io.Serializable;
+
+public class SerializableProperties implements Serializable {
+        private int a;
+        private int b;
+        private String c;
+
+        public SerializableProperties(int a, int b, String c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public int getA() {
+            return a;
+        }
+
+        public void setA(int a) {
+            this.a = a;
+        }
+
+        public int getB() {
+            return b;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
+
+        public String getC() {
+            return c;
+        }
+
+        public void setC(String c) {
+            this.c = c;
+        }
+}
