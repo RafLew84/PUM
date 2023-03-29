a7f2f270b483a8bc6bac55575682c633d97f69e0
Rafal Lewandkow <r.lewandkow@gmail.com>
2023-03-29 13:54:16 +0200

    mod1 - lab2 - SelectionTrackerBasics - Kotlin

--- 2023-2024/sourcecode/mod1/lab2/21SelectorBasicsKotlin/app/src/main/java/com/example/a21_selectorbasics_kotlin/MainActivity.kt
+++ 2023-2024/sourcecode/mod1/lab2/21SelectorBasicsKotlin/app/src/main/java/com/example/a21_selectorbasics_kotlin/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
         val numberListAdapter = NumberListAdapter(numberList)
         binding.recyclerView.adapter = numberListAdapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
