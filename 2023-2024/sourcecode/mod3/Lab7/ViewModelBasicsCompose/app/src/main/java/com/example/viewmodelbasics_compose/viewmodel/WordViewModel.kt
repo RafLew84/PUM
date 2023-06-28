6d6dade718449e4f0476a20d91025228e0bb1513
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-06-28 13:43:22 +0200

    lab 7.1 - ViewModelBasics - Compose data + viewmodel

---
+++ 2023-2024/sourcecode/mod3/Lab7/ViewModelBasicsCompose/app/src/main/java/com/example/viewmodelbasics_compose/viewmodel/WordViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.viewmodelbasics_compose.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.viewmodelbasics_compose.data.DataProvider
+
+class WordViewModel : ViewModel() {
+    private var _wordsList: MutableList<String> = mutableListOf()
+    val wordList: List<String>
+        get() = _wordsList
+
+    init {
+        reinitialize()
+    }
+
+    fun addWord(word: String){
+        _wordsList.add(word)
+        _wordsList.sort()
+    }
+
+    fun reinitialize(){
+        _wordsList.clear()
+        _wordsList.addAll(DataProvider.words)
+        _wordsList.sort()
+    }
+
+    fun clear(){
+        _wordsList.clear()
+    }
+}
