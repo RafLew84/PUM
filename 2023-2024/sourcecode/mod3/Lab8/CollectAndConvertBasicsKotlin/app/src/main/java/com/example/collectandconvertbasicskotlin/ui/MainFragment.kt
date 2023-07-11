e1f3301cff41c4c824d4376f181f0c0d86aba415
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-07-11 18:53:29 +0200

    invalidate caches fix + reimport

--- 2023-2024/sourcecode/mod3/Lab8/CollectAndConvertBasicsKotlin/app/src/main/java/com/example/collectandconvertbasicskotlin/ui/MainFragment.kt
+++ 2023-2024/sourcecode/mod3/Lab8/CollectAndConvertBasicsKotlin/app/src/main/java/com/example/collectandconvertbasicskotlin/ui/MainFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
