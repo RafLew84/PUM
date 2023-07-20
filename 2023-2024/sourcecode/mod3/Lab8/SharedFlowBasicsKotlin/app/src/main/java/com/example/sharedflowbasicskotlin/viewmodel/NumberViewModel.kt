44bdf134e153073c95bc33155c4a69c92bb90e9c
Rafal Lewandkow <r.lewandkow@gmail.com>
2023-07-20 18:44:44 +0200

    Update NumberViewModel.kt

--- 2023-2024/sourcecode/mod3/Lab8/SharedFlowBasicsKotlin/app/src/main/java/com/example/sharedflowbasicskotlin/viewmodel/NumberViewModel.kt
+++ 2023-2024/sourcecode/mod3/Lab8/SharedFlowBasicsKotlin/app/src/main/java/com/example/sharedflowbasicskotlin/viewmodel/NumberViewModel.kt
@@ -1,13 +1,28 @@
 package com.example.sharedflowbasicskotlin.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class NumberViewModel : ViewModel() {
 
-    private val _sharedFlow = MutableStateFlow(120)
-    val sharedFlow = _sharedFlow.asSharedFlow()
+    private var number = 0
+    private val _sharedFlow = MutableSharedFlow<Int>(1)
+    val sharedFlow: SharedFlow<Int> = _sharedFlow
+
+    init {
+        viewModelScope.launch {
+            while (true) {
+                _sharedFlow.emit(number++)
+                delay(500L)
+            }
+        }
+    }
 
 
 }
