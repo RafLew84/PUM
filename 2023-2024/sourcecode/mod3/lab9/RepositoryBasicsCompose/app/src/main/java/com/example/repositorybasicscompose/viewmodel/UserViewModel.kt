34315ca219d40e4200e80cab5b65e13795168803
Rafal Lewandkow <r.lewandkow@gmail.com>
2023-07-22 14:21:14 +0200

    lab 9.1 - Repository Basics Compose

--- 2023-2024/sourcecode/mod3/lab9/RepositoryBasicsCompose/app/src/main/java/com/example/repositorybasicscompose/viewmodel/UserViewModel.kt
+++ 2023-2024/sourcecode/mod3/lab9/RepositoryBasicsCompose/app/src/main/java/com/example/repositorybasicscompose/viewmodel/UserViewModel.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class UserViewModel() : ViewModel() {
+class UserViewModel : ViewModel() {
 
     private val userRepository = UserRepository()
 
