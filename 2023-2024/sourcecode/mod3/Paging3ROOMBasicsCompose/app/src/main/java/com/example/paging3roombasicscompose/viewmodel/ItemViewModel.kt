4f78deb745e9984a480ce0901852ce6e4d2b9cf5
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-08-25 14:21:25 +0200

    optimize imports

--- 2023-2024/sourcecode/mod3/Paging3ROOMBasicsCompose/app/src/main/java/com/example/paging3roombasicscompose/viewmodel/ItemViewModel.kt
+++ 2023-2024/sourcecode/mod3/Paging3ROOMBasicsCompose/app/src/main/java/com/example/paging3roombasicscompose/viewmodel/ItemViewModel.kt
@@ -8,9 +8,6 @@ import androidx.paging.cachedIn
 import com.example.paging3roombasicscompose.data.db.Item
 import com.example.paging3roombasicscompose.data.repository.ItemRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class ItemViewModel(application: Application) : ViewModel() {
