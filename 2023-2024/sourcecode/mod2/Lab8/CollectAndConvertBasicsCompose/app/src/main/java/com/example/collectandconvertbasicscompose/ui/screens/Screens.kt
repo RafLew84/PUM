bf447883197e9636b2758611e6a5f013331456b3
Rafal Lewandkow <r.lewandkow@gmail.com>
2023-09-24 18:09:45 +0200

    reorganize modules in sourcecode dir

--- 2023-2024/sourcecode/mod2/Lab8/CollectAndConvertBasicsCompose/app/src/main/java/com/example/collectandconvertbasicscompose/ui/screens/Screens.kt
+++
@@ -1,6 +0,0 @@
-package com.example.collectandconvertbasicscompose.ui.screens
-
-sealed class Screens(val route: String) {
-    object MainScreen : Screens("main_screen")
-    object SecondScreen : Screens("second_screen")
-}
--- 2023-2024/sourcecode/mod2/lab9/RepositoryBasicsCompose/app/src/main/java/com/example/repositorybasicscompose/model/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.repositorybasicscompose.model
-
-data class User(val firstName: String, val lastName: String)
