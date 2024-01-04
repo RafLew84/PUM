b7e079a7c83c4b869ed66d3cad193af064ebcb02
Rafal Lewandkow <r.lewandkow@gmail.com>
2024-01-04 08:53:51 +0100

    Update MainActivity.kt

--- 2023-2024/sourcecode/mod1/lab1/21CounterKotlin/app/src/main/java/com/example/a21counterkotlin/MainActivity.kt
+++ 2023-2024/sourcecode/mod1/lab1/21CounterKotlin/app/src/main/java/com/example/a21counterkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a21counterkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.a21counterkotlin.databinding.ActivityMainBinding
@@ -11,26 +12,14 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     //private val showCount: TextView by lazy{findViewById(R.id.show_count)}
-    //private val increaseButton: Button by lazy { findViewById(R.id.increase_button) }
     private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
         if (savedInstanceState != null)
             count = savedInstanceState.getInt("counter_state")
-
-//        //showCount.text = count.toString()
-//        binding.showCount.text = count.toString()
-//
-//        //increaseButton.setOnClickListener {
-//        binding.increaseButton.setOnClickListener {
-//            count++
-//            //showCount.text = count.toString()
-//            binding.showCount.text = count.toString()
-//        }
 
         binding.apply {
             showCount.text = count.toString()
